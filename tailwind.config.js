93e2833a6f9da73092847d07975061eafaad18cc
Julia Fitzgerald <104915352+jgfitzgerald@users.noreply.github.com>
2024-05-08 14:57:51 -0300

    integrate tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './speechdb/templates/speechdb/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
